7a0ce36e64d0677b1241d70e1537d9a05614efeb
William Chou <wchou@amazon.com>
2021-05-13 22:00:16 -0700

    add latency model and percent difference column

--- model.py
+++ model.py
@@ -20,20 +20,36 @@ def model(M, N, K):
 
 
 
-    # memory bound time
-    L2_CACHE_SIZE = 4500000
+    # buffer bandwidth bound time
+    # L2_CACHE_SIZE = 4500000
     MEMORY_BANDWIDTH = 6.53 * (10 ** 11)
     NUM_ELEMENTS_TO_READ = M * K + K * N
     FLOAT_SIZE = sys.getsizeof(float)
+    buffer_bandwidth_bound_time = NUM_ELEMENTS_TO_READ * FLOAT_SIZE / MEMORY_BANDWIDTH
+
+    # if NUM_ELEMENTS_TO_READ <= L2_CACHE_SIZE: # able to fit in L2 cache
+    #     memory_bound_time = NUM_ELEMENTS_TO_READ * FLOAT_SIZE / MEMORY_BANDWIDTH
+    # else:
+    #     memory_bound_time = NUM_ELEMENTS_TO_READ * FLOAT_SIZE / MEMORY_BANDWIDTH * NUM_ELEMENTS_TO_READ/L2_CACHE_SIZE
+
+
+
+    # memory latency bound time (read/write from memory)
+    L2_CACHE_SIZE = 4500000
+    MEMORY_CLOCK = 1.7 * (10 ** 11)
+    NUM_ELEMENTS_TO_READ = M*K + K*N
+    FLOAT_SIZE = sys.getsizeof(float)
+    memory_latency_time = -1
     if NUM_ELEMENTS_TO_READ <= L2_CACHE_SIZE: # able to fit in L2 cache
-        memory_bound_time = NUM_ELEMENTS_TO_READ * FLOAT_SIZE / MEMORY_BANDWIDTH
+        memory_latency_time = NUM_ELEMENTS_TO_READ * FLOAT_SIZE * 8 / MEMORY_CLOCK # times 8 because clock is given in gigabits
     else:
-        memory_bound_time = NUM_ELEMENTS_TO_READ * FLOAT_SIZE / MEMORY_BANDWIDTH * NUM_ELEMENTS_TO_READ/L2_CACHE_SIZE
+        memory_latency_time = ((L2_CACHE_SIZE * FLOAT_SIZE * 8 / MEMORY_CLOCK) * (NUM_ELEMENTS_TO_READ/L2_CACHE_SIZE)) + ((NUM_ELEMENTS_TO_READ - L2_CACHE_SIZE*(NUM_ELEMENTS_TO_READ/L2_CACHE_SIZE)) * FLOAT_SIZE * 8 / MEMORY_CLOCK) 
+
 
     # Return the maximum of the compute bound time, bandwidth bound time, and memory bound time
     # print(compute_bound_time)
     # print(memory_bound_time)
-    return max(compute_bound_time, memory_bound_time)
+    return max(compute_bound_time, buffer_bandwidth_bound_time, memory_latency_time)
 
 """Compares our model's CUDNN with DeepBench's CUDNN
 
@@ -46,7 +62,7 @@ def main():
     file1 = open('deepbench_benchmarks.txt', 'r')
     Lines = file1.readlines()
     mean_squared_error = 0
-    print('{:<10s}{:<20s}{:<30s}{:<40s}{:<50s}'.format('M','N','K','CUDNN execution time','Our Model Estimate'))
+    print('{:<10s}{:<20s}{:<30s}{:<40s}{:<50s}{:<60s}'.format('M','N','K','CUDNN execution time','Our Model Estimate', 'Percent Difference (%)'))
     for line in Lines[1:]:
         contents = line.split()
         m = int(contents[0])
@@ -55,8 +71,9 @@ def main():
 
         time_cudnn = float(contents[-1])
         our_model_time = model(m, n, k)
+        percent_difference = abs((our_model_time - time_cudnn) / time_cudnn) * 100
         mean_squared_error += ((time_cudnn - our_model_time)**2)
-        print('{:<10s}{:<20s}{:<30s}{:<40s}{:<50s}'.format(str(m),str(n),str(k),str(time_cudnn),str(our_model_time)))
+        print('{:<10s}{:<20s}{:<30s}{:<40s}{:<50s}{:<60s}'.format(str(m),str(n),str(k),str(time_cudnn),str(our_model_time),str(percent_difference)))
     #print(mean_squared_error)
     #print(len(Lines[1:]))
     mean_squared_error = mean_squared_error / len(Lines[1:])
