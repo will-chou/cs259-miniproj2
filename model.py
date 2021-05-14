1e03653951a67b10f6fba322491000ef4ad89bf2
Jim Zhou <40669892+jimzhou112@users.noreply.github.com>
2021-05-13 22:52:22 -0400

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import sys
+
 """Our model for CUDNN
 
 Take the maximum of compute bound time, buffer bandwidth bound time, and the memory bound time
@@ -10,7 +12,28 @@ Memory bound time calculated .....
 
 """
 def model(M, N, K):
-    return 0.05 # This is what we would need to change for the project
+    # compute bound time
+    NUM_CORES = 5120
+    CORE_CLOCK = 1200
+
+    compute_bound_time = (2*K*M*N/NUM_CORES)/CORE_CLOCK
+
+
+
+    # memory bound time
+    L2_CACHE_SIZE = 4500000
+    MEMORY_BANDWIDTH = 6.53 * (10 ** 11)
+    NUM_ELEMENTS_TO_READ = M * K + K * N
+    FLOAT_SIZE = sys.getsizeof(float)
+    if NUM_ELEMENTS_TO_READ <= L2_CACHE_SIZE: # able to fit in L2 cache
+        memory_bound_time = NUM_ELEMENTS_TO_READ * FLOAT_SIZE / MEMORY_BANDWIDTH
+    else:
+        memory_bound_time = NUM_ELEMENTS_TO_READ * FLOAT_SIZE / MEMORY_BANDWIDTH * NUM_ELEMENTS_TO_READ/L2_CACHE_SIZE
+
+    # Return the maximum of the compute bound time, bandwidth bound time, and memory bound time
+    # print(compute_bound_time)
+    # print(memory_bound_time)
+    return max(compute_bound_time, memory_bound_time)
 
 """Compares our model's CUDNN with DeepBench's CUDNN
 
@@ -32,10 +55,11 @@ def main():
 
         time_cudnn = float(contents[-1])
         our_model_time = model(m, n, k)
-        mean_squared_error += (time_cudnn - our_model_time)**2
+        mean_squared_error += ((time_cudnn - our_model_time)**2)
         print('{:<10s}{:<20s}{:<30s}{:<40s}{:<50s}'.format(str(m),str(n),str(k),str(time_cudnn),str(our_model_time)))
-
-    mean_squared_error /= len(Lines[1:])
+    print(mean_squared_error)
+    print(len(Lines[1:]))
+    mean_squared_error = mean_squared_error / len(Lines[1:])
     print("Total mean squared error: " + str(mean_squared_error))
 
 
